47efec8733b71f55a1f9da892fc062b8b9b519e2
Aleksandr Kulikov <akulikov@Aleksandrs-MacBook-Pro.local>
2013-08-24 12:45:55 +0400

    python3, пока все криво

--- convert.py
+++ convert.py
@@ -4,16 +4,15 @@ from os import path
 import sys
 
 def main(curDir=path.abspath('.')):
-    for i in filter(lambda x: path.isdir(x), os.listdir(curDir)):
-	    for j in filter(lambda x: path.splitext(x)[1] in ['.avi','.mkv','.ogv','.wmv','.flv'],os.listdir(i)):
-			print curDir
+	for i in filter(lambda x: path.isdir(x), os.listdir(curDir)):
+		for j in filter(lambda x: path.splitext(x)[1] in ['.avi','.mkv','.ogv','.wmv','.flv'],os.listdir(i)):
 			os.system('HandBrakeCLI -i "./'+i+'/'+j+'" -o "./'+i+'/'+path.splitext(j)[0]+'.mp4"')
 			os.system('rm -f "./'+i+'/'+j+'"')
-    listDir = filter(lambda x: path.splitext(x)[1] in ['.avi','.ogv','.mkv', '.wmv', '.flv'], os.listdir(curDir))
-    print os.listdir(curDir)
-    for i in listDir:
-        os.system('HandBrakeCLI -i "' + i + '" -o "' + path.splitext(i)[0] + '.mp4"')
-        os.system('rm -f "' + i + '"')
+	listDir = filter(lambda x: path.splitext(x)[1] in ['.avi','.ogv','.mkv', '.wmv', '.flv'], os.listdir(curDir))
+	print (os.listdir(curDir))
+	for i in listDir:
+		os.system('HandBrakeCLI -i "' + i + '" -o "' + path.splitext(i)[0] + '.mp4"')
+		os.system('rm -f "' + i + '"')
 
 if '__main__'==__name__:
 	if sys.argv.__len__()==1:
--- podfeedgen.py
+++ podfeedgen.py
@@ -1,39 +1,73 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import PyRSS2Gen as rss
 import os
 from os import path
 import sys
 import datetime
-import operator
 from functools import reduce
-import urllib2
-
 import convert
+
+
+def esc(s):
+    sym = {
+        # "%": '%25',
+        " ": '%20',
+        "<": '%3C',
+        ">": '%3E',
+        "#": '%23',
+
+        "{": '%7B',
+        "}": '%7D',
+        '|': '%7C',
+        '\\': '%5C',
+        '^': '%5E',
+        '~': '%7E',
+        '[': '%5B',
+        ']': '%5D',
+        '`': '%60',
+        ';': '%3B',
+        '/': '%2F',
+        '?': '%3F',
+        ':': '%3A',
+        '@': '%40',
+        '=': '%3D',
+        '&': '%26',
+        '$': '%24',
+    }
+
+    for i in sym:
+        s = s.replace(i, sym[i])
+    return s
+
+
 dir = sys.argv[1]
-print os.listdir(dir)
+print(os.listdir(dir))
 url = sys.argv[2]
-myDir=os.path.abspath('.')
+myDir = os.path.abspath('.')
 os.chdir(dir)
 convert.main(dir)
-listDir = filter(lambda x: path.isdir(x), os.listdir(dir))
-if listDir!=[]:
-    listDir=reduce(lambda x,y: x+y,map(lambda x: map(lambda k:x+'/'+k,os.listdir(x)),listDir))
-files = filter(lambda x: path.splitext(x)[1] in [".mp3",".m4v", '.mp4', ".mov"],listDir+ os.listdir(dir))
+listDir = list(filter(lambda x: path.isdir(x), os.listdir(dir)))
+if listDir:
+    print(listDir)
+    lm = list(map(lambda x: list(map(lambda k: x + '/' + k, os.listdir(x))), listDir))
+    print(lm)
+    listDir = reduce(lambda x, y: x + y, lm)
+files = list(filter(lambda x: path.splitext(x)[1] in [".mp3", ".m4v", '.mp4', ".mov"], listDir + os.listdir(dir)))
 myItems = [(rss.RSSItem(
-    title=n.decode('utf-8'),
+    title=n,
     description='',
-    enclosure=rss.Enclosure(url + '/' + urllib2.quote(n.decode('utf-8')), 0, "audio/mpeg")
+    enclosure=rss.Enclosure(url + '/' + esc(n), 0, "audio/mpeg")
 )) for n in files]
 feed = rss.RSS2(
-    #title=str.decode(path.basename(path.abspath(dir)), 'utf-16'),
-    title=path.basename(path.abspath(dir).decode('utf-8')),
+    title=path.basename(path.abspath(dir)),
     link="http://unnotigkeit.ya.ru",
     description="",
     lastBuildDate=datetime.datetime.now(),
     items=myItems
 )
-dbpath=os.path.expanduser("~/Dropbox/Public/")
-feed.write_xml(open(dir +'/'+ path.basename(path.abspath(dir)) + ".xml", "w"))
-print(url + '/' + urllib2.quote(path.basename(path.abspath(dir))) + ".xml")
+
+feed.write_xml(open((dir + '/' + path.basename(path.abspath(dir)) + ".xml"), "w"),"utf-8")
+print(url + '/' + esc(path.basename(path.abspath(dir))) + ".xml")
 os.system('python -m SimpleHTTPServer 3000')
 os.chdir(myDir)
