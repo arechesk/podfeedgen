578e43acea1b76c5abe05da5377d7c71a180323b
Alex Kulikoff <unnotigkeit@mail.ru>
2016-11-29 00:33:54 +0300

    commit

--- convert.py
+++ convert.py
@@ -5,13 +5,13 @@ import sys
 
 def main(curDir=path.abspath('.')):
 	for i in filter(lambda x: path.isdir(x), os.listdir(curDir)):
-		for j in filter(lambda x: path.splitext(x)[1] in ['.avi','.mkv','.ogv','.wmv','.flv'],os.listdir(i)):
-			os.system('HandBrakeCLI -i "./'+i+'/'+j+'" -o "./'+i+'/'+path.splitext(j)[0]+'.mp4"')
+		for j in filter(lambda x: path.splitext(x)[1] in ['.avi','.mpg','.mkv','.ogv','.wmv','.flv','.ogg'],os.listdir(i)):
+			os.system('HandBrakeCLI -i "./'+i+'/'+j+'" -o "./'+i+'/'+path.splitext(j)[0]+'.mov"')
 			os.system('rm -f "./'+i+'/'+j+'"')
-	listDir = filter(lambda x: path.splitext(x)[1] in ['.avi','.ogv','.mkv', '.wmv', '.flv'], os.listdir(curDir))
+	listDir = filter(lambda x: path.splitext(x)[1] in ['.FLV','.avi','.mpg','.ogv','.mkv', '.wmv', '.flv','.ogg'], os.listdir(curDir))
 	print (os.listdir(curDir))
 	for i in listDir:
-		os.system('HandBrakeCLI -i "' + i + '" -o "' + path.splitext(i)[0] + '.mp4"')
+		os.system('HandBrakeCLI -i "' + i + '" -o "' + path.splitext(i)[0] + '.mov"')
 		os.system('rm -f "' + i + '"')
 
 if '__main__'==__name__:
--- podfeedgen.py
+++ podfeedgen.py
@@ -7,46 +7,49 @@ import datetime
 import convert
 from functools import reduce
 from os import path
+import urllib.parse
+import random
+port=3000+random.randint(0,10)
+print(port)
+esc=urllib.parse.quote
+# def esc(s):
+#     sym = {
+#         # "%": '%25',
+#         " ": '%20',
+#         "<": '%3C',
+#         ">": '%3E',
+#         "#": '%23',
 
+#         "{": '%7B',
+#         "}": '%7D',
+#         '|': '%7C',
+#         '\\': '%5C',
+#         '^': '%5E',
+#         '~': '%7E',
+#         '[': '%5B',
+#         ']': '%5D',
+#         '`': '%60',
+#         ';': '%3B',
+#         '/': '%2F',
+#         '?': '%3F',
+#         ':': '%3A',
+#         '@': '%40',
+#         '=': '%3D',
+#         '&': '%26',
+#         '$': '%24',
+#     }
 
-def esc(s):
-    sym = {
-        # "%": '%25',
-        " ": '%20',
-        "<": '%3C',
-        ">": '%3E',
-        "#": '%23',
-
-        "{": '%7B',
-        "}": '%7D',
-        '|': '%7C',
-        '\\': '%5C',
-        '^': '%5E',
-        '~': '%7E',
-        '[': '%5B',
-        ']': '%5D',
-        '`': '%60',
-        ';': '%3B',
-        '/': '%2F',
-        '?': '%3F',
-        ':': '%3A',
-        '@': '%40',
-        '=': '%3D',
-        '&': '%26',
-        '$': '%24',
-    }
-
-    for i in sym:
-        s = s.replace(i, sym[i])
-    return s
+#     for i in sym:
+#         s = s.replace(i, sym[i])
+#     return s
 
 
 def _cmp(x):
     import re
-    if len(re.findall(r'.*?(\d+).*(.mp3|.m4v|.mp4|.mov)$',x))==0:
+    if len(re.findall(r'.*?(\d+).*(.mp3|.m4v|.M4V|.mp4|.MP4|.mov)$', x)) == 0:
         return 0
     else:
-        return int(re.findall(r'.*?(\d+).*(.mp3|.m4v|.mp4|.mov)$',x)[0][0])
+        return int(re.findall(r'.*?(\d+).*(.mp3|.m4v|.M4V|.mp4|.MP4|.mov)$', x)[0][0])
 
 
 dir = sys.argv[1]
@@ -59,14 +62,14 @@ listDir = list(filter(lambda x: path.isdir(x), os.listdir(dir)))
 if listDir:
     print(listDir)
     lm = list(map(lambda x: list(map(lambda k: x + '/' + k, os.listdir(x))), listDir))
-    print(lm)
+    print("lm:{}\n *********************".format(lm))
     listDir = reduce(lambda x, y: x + y, lm)
-files = list(filter(lambda x: path.splitext(x)[1] in [".mp3", ".m4v", '.mp4', ".mov"], listDir + os.listdir(dir)))
+files = list(filter(lambda x: path.splitext(x)[1] in [".mp3",".M4V", ".m4v", '.mp4',".MP4", ".mov"], listDir + os.listdir(dir)))
 files.sort(key=_cmp)
 myItems = [(rss.RSSItem(
     title=n,
     description='',
-    enclosure=rss.Enclosure(url + '/' + esc(n), 0, "audio/mpeg")
+    enclosure=rss.Enclosure(url+':'+str(port) + '/' + esc(n), 0, "audio/mpeg")
 )) for n in files]
 myItems.reverse()
 feed = rss.RSS2(
@@ -78,6 +81,6 @@ feed = rss.RSS2(
 )
 
 feed.write_xml(open((dir + '/' + path.basename(path.abspath(dir)) + ".xml"), "w"), "utf-8")
-print(url + '/' + esc(path.basename(path.abspath(dir))) + ".xml")
-os.system('python -m SimpleHTTPServer 3000')
-os.chdir(myDir)
+print(url+':'+str(port)+ '/' + esc(path.basename(path.abspath(dir))) + ".xml")
+import http.server
+os.system('python -m SimpleHTTPServer {}'.format(port))
